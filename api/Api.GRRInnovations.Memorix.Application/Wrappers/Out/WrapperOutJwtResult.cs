f18cb123c22cebcefc966c626d05470f621a5b73
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2025-10-26 20:24:19 -0300

    add wrraper jwt

---
+++ api/Api.GRRInnovations.Memorix.Application/Wrappers/Out/WrapperOutJwtResult.cs
@@ -0,0 +1,46 @@
+﻿using Api.GRRInnovations.Memorix.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Api.GRRInnovations.Memorix.Application.Wrappers.Out
+{
+    public class WrapperOutJwtResult : WrapperBase<JwtResultModel, WrapperOutJwtResult>
+    {
+        public WrapperOutJwtResult() : base(null) { }
+
+        public WrapperOutJwtResult(JwtResultModel data) : base(data) { }
+
+        [JsonPropertyName("access_token")]
+        public string AccessToken
+        {
+            get => Data.AccessToken;
+            set => Data.AccessToken = value;
+        }
+
+        [JsonPropertyName("expire")]
+        public double Expire
+        {
+            get => Data.Expire;
+            set => Data.Expire = value;
+        }
+
+        [JsonPropertyName("type")]
+        public string Type
+        {
+            get => Data.Type;
+            set => Data.Type = value;
+        }
+
+        public new static async Task<WrapperOutJwtResult> From(JwtResultModel model)
+        {
+            var wrapper = new WrapperOutJwtResult();
+            await wrapper.Populate(model).ConfigureAwait(false);
+
+            return wrapper;
+        }
+    }
+}
