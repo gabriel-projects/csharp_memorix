37a002ea2e8884924c7ce526d74020505d643542
Gabriel Ribeiro Rossi <gabriel.rossi@sebsa.com.br>
2025-11-01 15:08:05 -0300

    add loggin with serilog

--- api/Api.GRRInnovations.Memorix/Program.cs
+++ api/Api.GRRInnovations.Memorix/Program.cs
@@ -1,14 +1,37 @@
+using Serilog;
+
 namespace Api.GRRInnovations.Memorix
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File("logs/memorix-.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting Memorix API");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Memorix API terminated unexpectedly");
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
